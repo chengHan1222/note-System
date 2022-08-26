a24cff7e174aeecf296a61298f6036bbe7bfbcbe
LuiliXain <qaz8155699@gmail.com>
2022-08-26 14:32:42 +0800

    update(2022/8/26 02:32)

---
+++ src/page/MainPage/FileBar/FileBtn/index.jsx
@@ -0,0 +1,88 @@
+import React, { useRef } from "react";
+import style from "./index.module.scss";
+
+
+class FileBtn extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			fileName: props.fileName,
+			isSelect: props.isSelect,
+			isNaming: props.isNaming,
+			setfileName: props.setfileName,
+			status: {},	
+		}
+		this.handleInput = this.handleInput.bind(this);
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		let status = {};
+		if (props.isSelect===`${props.folderName}_fileBtn${props.id}`) {
+			if (props.focusSpace === "FileBar") {
+				if (props.isNaming === false) {
+					status = {backgroundColor: "rgba(0, 0, 255, 0.3)",
+						border: "1px solid blue"};
+				} else { 
+					status = {backgroundColor: "rgb(85, 94, 98)",
+						border: "1px solid rgba(85, 94, 98, 0.5)"};
+				}
+			} else {
+				status = {backgroundColor: "rgb(85, 94, 98)",
+					border: "1px solid rgba(85, 94, 98, 0.5)"};
+			}
+		} else if (props.isNaming) {
+			
+		}
+		return {status: status};
+	}
+
+	handleInput(event) {
+		console.log(event)
+		if (this.state.isNaming) {
+			this.props.setfileName(event.target.outerText);
+		}
+	}
+
+	render() {
+		return(
+			<div id={`${this.props.folderName}_fileBtn${this.props.id}`}
+				className={style.fileBtnStyle}
+				style={this.state.status}
+			>
+				<img src={require("../../../../assets/tIcon.png")} className={style.fileBtnIcon}/>
+				<div className={style.fileBtnTitle} 
+					contentEditable={(this.state.isNaming)? true: false}
+					style={(this.state.isNaming)? 
+						{border: "1px solid  blue"}: {}}
+					onInput={this.handleInput}
+					onKeyDown={this.props.keyDown}
+				>{this.state.fileName}</div>
+			</div>
+		)
+	}
+}
+
+// const fileBtn = (index, folderName, fileName, isNaming, isSelect, focusSpace, setfileName) => {
+// 	const enterfileName = (event) => {
+// 		setfileName(event.target.outerText)
+// 	}
+
+// 	return (
+// 		(isNaming === false)? 
+// 		<FileBtn key={`fileBtn${fileName}${index}`}             
+// 			folderName={folderName}
+// 			fileName={fileName}
+// 			isNaming={isNaming}
+// 			isSelect={isSelect}
+// 			focusSpace={focusSpace}
+// 		/>: 
+// 		<div key={`fileBtn${index}`} 
+// 			id={"normal_fileBtn0"}
+// 			contentEditable={true} 
+// 			className={style.fileBtnTitle} 
+// 			onInput={enterfileName}
+// 		></div>
+// 	)
+// }
+
+export default FileBtn;
--- src/page/MainPage/index.jsx
+++ src/page/MainPage/index.jsx
@@ -4,25 +4,70 @@ import style from './index.module.scss'
 import ToolBar from './ToolBar';
 import FileBar from './FileBar';
 import EditFrame from './EditFrame';
-
+import FileRightClickBlock from './FileBar/fileRightClickBlock';
 
 export default class index extends Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
-			title: 'title........'
+			strTitle: 'title........',
+			strFocusFile: "",
+			strFocusSpace: "",
+			
+			files: [
+				{folder: "favorite",
+				files: [
+					{fileName: "first",
+					fileData: `List  0/*/List  1/*/List  2/*/List  3/*/List  4/*/List  5/*/List  6/*/List  7/*/<strong>123</strong>/*/`,
+					isNaming: false}, 
+					{fileName: "second", 
+					fileData: `List  0/*/List  1/*/List  2/*/List  3/*/List  4/*/List  5/*/List  6/*/List  7/*/<strong>123</strong>/*/`,
+					isNaming: false}
+				],
+				isNaming: false},
+				{folder: "normal",
+				files: [
+					{fileName: "third",
+					fileData: `List  0/*/List  1/*/List  2/*/List  3/*/List  4/*/List  5/*/List  6/*/List  7/*/<strong>123</strong>/*/`,
+					isNaming: false}, 
+					{fileName: "forth", 
+					fileData: `List  0/*/List  1/*/List  2/*/List  3/*/List  4/*/List  5/*/List  6/*/List  7/*/<strong>123</strong>/*/`,
+					isNaming: false}
+				],
+				isNaming: false}
+			]
 		}
 	}
 
-	change() {
-		this.setState({title: "banana"});
+	setFocusFile(strFocusFile) {
+		this.setState({strFocusFile: strFocusFile});
+		console.log("strFocusFile", strFocusFile)
+	}
+
+	handleClick(event) {
+		let target = this.findFocusSpace(event.target);
+		this.setState({focusSpace: target})
+	}
+
+	findFocusSpace(target) {
+		while (!target.parentNode.id.includes("mainSpace")) {
+			target = target.parentNode;
+		}
+		return (target.className.includes("editFrame"))? "EditFrame": "FileBar";
 	}
 
 	render() {
 		return (
-			<div className={style.mainPage}>
-				<FileBar title={this.state.title}></FileBar>
+			<div id={"mainSpace"}
+				className={style.mainPage}
+				onClick={this.handleClick.bind(this)}
+				onContextMenu={this.handleClick.bind(this)}
+			>
+				<FileBar title={this.state.strTitle} 
+					files={this.state.files} 
+					funSetFocusFile={this.setFocusFile.bind(this)}
+					focusSpace={this.state.focusSpace}
+				/>
 
 				<div className={style.editFrame}>
 					<ToolBar></ToolBar>
