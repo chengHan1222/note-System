1c95cb50fe172038b159329dff25e18bc765e869
chengHan <75972336+chengHan1222@users.noreply.github.com>
2022-12-13 15:24:36 +0800

    update 2022/12/13 pdf upload

--- src/page/MainPage/ToolBar/index.jsx
+++ src/page/MainPage/ToolBar/index.jsx
@@ -4,29 +4,77 @@ import ReactFileReader from 'react-file-reader';
 import style from './light.module.scss';
 import darkmode from './dark.module.scss';
 
-import { Divider, Dropdown, Modal, Popover, Space } from 'antd';
+import { Button, Divider, Dropdown, Modal, Popover } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
 import { faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 import OpenCamera from './OpenCamera';
-import RecogBtn from './RecogBtn';
 import SunEditor from './SunEditor';
+import styled from 'styled-components';
 
 import EditManager from '../../../tools/EditFrame';
 import classDrawBoard from '../../../tools/DrawBoard';
 import TextEditor from '../../../tools/TextEditor';
 import StepControl from '../../../tools/StepControl';
-
 import Controller from '../../../tools/Controller';
 import UserData from '../../../tools/UserData';
 
+const CircleBtn = styled.div`
+	width: 200px;
+	display: flex;
+	flex-wrap: wrap;
+
+	.colorSpan,
+	.colorSelector {
+		width: 30px;
+		height: 30px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 5px;
+		border-radius: 50%;
+		box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.7);
+		color: white;
+		font-size: 25px;
+		cursor: pointer;
+	}
+
+	.comfirmDiv {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		margin-top: 10px;
+	}
+`;
+
+const Upload = styled.div`
+	margin-right: 15px;
+	svg {
+		width: 34px;
+		height: 34px;
+	}
+`;
+
 const { useEffect, useState, useRef } = React;
 
+const PDFJS = window.pdfjsLib;
+
 const ToolBar = (props) => {
+	const colorCircleBtn = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', '#DABEA7', 'black', 'white'];
+	const imagesList = [];
+
 	const childRef = useRef();
 	const canvasRef = useRef();
+	const colorInput = useRef();
+	const pdfUploadRef = useRef();
 	const [isCamaraOpen, setCamaraOpen] = useState(false);
+	const [color, setColor] = useState('black');
+	const [isSelecting, setSelecting] = useState(false);
+	const [palette, setPalette] = useState('white');
+
+	const [pdf, setPdf] = React.useState('');
 	const css = props.style ? darkmode : style;
 
 	useEffect(() => {
@@ -39,6 +87,15 @@ const ToolBar = (props) => {
 			}
 		});
 	}, []);
+
+	useEffect(() => {
+		pdf &&
+			renderPage().then(() => {
+				imagesList.forEach((element) => {
+					handleImageFiles(element);
+				});
+			});
+	}, [pdf]);
 
 	const drawNewPicture = (color) => {
 		const canvas = canvasRef.current;
@@ -63,10 +120,9 @@ const ToolBar = (props) => {
 
 		let EditList = EditManager.lisEditList[EditManager.focusIndex + 1];
 		EditList.type = 'image';
+		EditList.imgSrc = file.base64;
 
 		if (file.fileList) {
-			EditList.imgSrc = file.base64;
-
 			Controller.uploadImg(UserData.userId, file.fileList[0]).then((response) => {
 				EditList.strHtml = response.imgId;
 				UserData.setImgs(response.data.img);
@@ -75,8 +131,6 @@ const ToolBar = (props) => {
 				props.saveFile();
 			});
 		} else {
-			EditList.imgSrc = file.base64;
-
 			Controller.uploadImg(UserData.userId, Controller.dataURItoBlob(file.base64)).then((response) => {
 				UserData.setImgs(response.data.img);
 				EditList.strHtml = response.imgId;
@@ -101,10 +155,81 @@ const ToolBar = (props) => {
 		});
 	};
 
+	const handlePDFFiles = async (event) => {
+		try {
+			const file = event.target.files[0];
+			const uri = URL.createObjectURL(file);
+			let _PDF_DOC = await PDFJS.getDocument({ url: uri });
+			setPdf(_PDF_DOC);
+			pdfUploadRef.current.value = '';
+		} catch (error) {
+			alert(error.message);
+		}
+	};
+
+	const renderPage = async () => {
+		const canvas = canvasRef.current;
+
+		for (let i = 1; i <= pdf.numPages; i++) {
+			let page = await pdf.getPage(i);
+			let viewport = page.getViewport({ scale: 1 });
+			canvas.width = viewport.width;
+			canvas.height = viewport.height;
+			let render_context = {
+				canvasContext: canvas.getContext('2d'),
+				viewport: viewport,
+			};
+			await page.render(render_context).promise;
+			imagesList.push({ base64: canvas.toDataURL() });
+		}
+	};
+
 	const updateEditList = (List) => {
 		EditManager.readFile(List);
 	};
-	const imageBackground = <div>123</div>;
+	const imageBackground = (
+		<CircleBtn>
+			{colorCircleBtn.map((element) => {
+				return (
+					<div
+						className="colorSelector"
+						key={'colorBtn' + element}
+						style={{ color: element === 'white' ? 'black' : '', backgroundColor: element }}
+						onClick={() => {
+							setSelecting(false);
+							setColor(element);
+						}}
+					>
+						{color === element ? '✓' : ''}
+					</div>
+				);
+			})}
+			<span
+				className="colorSpan"
+				style={{ color: 'black', backgroundColor: palette, fontSize: isSelecting ? '' : '38px', paddingBottom: isSelecting ? '' : '5px' }}
+				onClick={() => {
+					setSelecting(true);
+					colorInput.current.click();
+				}}
+			>
+				{isSelecting ? '✓' : '+'}
+			</span>
+			<div style={{ position: 'relative' }}>
+				<input
+					type="color"
+					ref={colorInput}
+					style={{ visibility: 'hidden', position: 'absolute', left: '-45px', top: '-20px', zIndex: '2' }}
+					onChange={(e) => {
+						setColor(e.target.value);
+						setPalette(e.target.value);
+					}}
+				/>
+			</div>
+			<div className="comfirmDiv">
+				<Button onClick={(e) => drawNewPicture(color)}>確認</Button>
+			</div>
+		</CircleBtn>
+	);
 
 	const pictureItem = [
 		{
@@ -121,11 +246,9 @@ const ToolBar = (props) => {
 		},
 		{
 			label: (
-				<div onClick={() => drawNewPicture('red')}>123</div>
-				// onClick={() => drawNewPicture('red')}
-				// <Popover placement="bottom" content={imageBackground} title="Title">
-				// 	新增畫布
-				// </Popover>
+				<Popover placement="bottom" content={imageBackground} title="背景顏色">
+					新增畫布
+				</Popover>
 			),
 			key: '3',
 		},
@@ -170,8 +293,14 @@ const ToolBar = (props) => {
 			<div className={css.iconBar}>
 				<canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
 
+				<Upload>
+					{/* <ReactFileReader fileTypes={['.pdf']} base64={true} multipleFiles={false} handleFiles={handlePDFFiles}> */}
+					<UploadOutlined onClick={() => pdfUploadRef.current.click()} />
+					<input ref={pdfUploadRef} type="file" accept="application/pdf" hidden onChange={handlePDFFiles} />
+					{/* </ReactFileReader> */}
+				</Upload>
+
 				<Dropdown menu={{ items: pictureItem }} trigger={['click']}>
-					{/* <img src={require('../../../assets/OCR_light.png')}/> */}
 					<FontAwesomeIcon icon={faImage} />
 				</Dropdown>
 
